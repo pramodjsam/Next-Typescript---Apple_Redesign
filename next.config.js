ed33a7e9dded8df174bf4a37810c08a700be3eb6
Pramod Jacob Sam <pramodjsam@gmail.com>
2022-11-15 20:36:24 -0500

    Init commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "rb.gy",
+      "cdn.sanity.io",
+      "www.gravatar.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
